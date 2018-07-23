0848619ce48ebe5e92f4286787a5e2bbf59faf97
Ricky Collins <richardcollins52317@gmail.com>
2018-07-22 22:16:41 -0400

    testing ENV files

--- app.rb
+++ app.rb
@@ -5,12 +5,33 @@ enable :sessions
 
 get '/' do
 
-  erb :index, :layout => :layout
+  erb :index, :layout => :layout, locals: {err_msg: ''}
 end
 
 post '/login' do
-  
-redirect '/calc'
+first_name = params[:first_name]
+last_name = params[:last_name]
+user_name = params[:user_name]
+password = params[:password]
+
+
+
+valid_name = ENV['user_name'].split(' ')
+valid_pwd = ENV['password'].split(' ')
+
+if valid_name.include?(user_name)
+  if valid_pwd.include?(password)
+    if valid_name.index(user_name) == valid_pwd.index(password)
+      redirect '/calc'
+    else
+      erb :index, :layout => :layout, locals: {err_msg: 'Username and Password do not match'}
+    end
+  else
+    erb :index, :layout => :layout, locals: {err_msg: 'Invalid Password'}
+  end
+elsif valid_pwd.include?(password)
+  erb :index, :layout => :layout, locals: {err_msg: 'Invalid Username'}
+end
 end
 
 get '/calc' do
